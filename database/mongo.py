d7bd4b1b613a194d9dab3843fcecf3d4d1ad58cb
ErVarunGupta <varungupta0994@gmail.com>
2026-01-07 19:59:10 +0530

    update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,10 +1,15 @@
 
 from pymongo import MongoClient
 import os
+import streamlit as st
 
-MONGO_URI = os.getenv("mongodb://localhost:27017/")
+# MONGO_URI = os.getenv("mongodb://localhost:27017/")
+MONGO_URI = st.secrets["MONGO_URI"]
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    serverSelectionTimeoutMS=5000
+)
 db = client["smart_energy_ai"]
 
 energy_col = db["energy_usage"]
